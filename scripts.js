5224a463af5146380fff30c3cb1b4f915bb0f21b
Neonixes <rikokazak69@gmail.com>
2024-11-26 20:11:18 +0500

    Fix files after merge

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+function showAlert() {
+    alert('Добро пожаловать на наш сайт! Исследуйте всё, что мы приготовили.');
+}
+
+function showCategory(category) {
+    alert(`Вы выбрали категорию: ${category}. Скоро добавим больше информации!`);
+}
