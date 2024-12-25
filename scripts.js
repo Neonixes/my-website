c56740df2d6431a81f244763690eec346349c95f
Neonixes <rikokazak69@gmail.com>
2024-12-25 22:49:46 +0500

    Initial commit

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// scripts.js
-function showAlert() {
-    alert('Добро пожаловать на наш сайт! Исследуйте всё, что мы приготовили.');
-}
-
-function showCategory(category) {
-    alert(`Вы выбрали категорию: ${category}. Скоро добавим больше информации!`);
-}
